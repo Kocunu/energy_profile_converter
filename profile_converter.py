0a452854ce2fa2c8fb397dc612753819ab0fdd2a
Kocunu <alican.koc@gmx.at>
2023-10-07 19:37:14 +0200

    Files organisation and first some line of starting code to read the json file

--- profile_converter.py
+++ profile_converter.py
@@ -1,9 +1,7 @@
 import json
 
-with open('example.json', 'r') as json_file:
+with open('input_files/example.json', 'r') as json_file:
     json_object = json.load(json_file)
     DumptJsonFile = json.dumps(json_object, indent=1)
     print(DumptJsonFile)
-    
-
 
